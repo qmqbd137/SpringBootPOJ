3a8db0809135165201d76e5939bb9cb1a4cbd4f2
BdQmq <124289241@qq.com>
2022-01-08 20:38:34 +0800

    初始项目创建

---
+++ src/main/java/com/qmq/springbootpoj/SpringBootPojApplication.java
@@ -0,0 +1,14 @@
+package com.qmq.springbootpoj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScans;
+
+@SpringBootApplication
+public class SpringBootPojApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootPojApplication.class, args);
+    }
+
+}
