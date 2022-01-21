5a41b99aced237621225ed73dca8bad6d49c8c1b
xiaobendu <124289241@qq.com>
2022-01-21 17:58:46 +0800

    springboot整合apolloclient实现远程配置仓库读取

--- src/main/java/com/qmq/springbootpoj/SpringBootPojApplication.java
+++ src/main/java/com/qmq/springbootpoj/SpringBootPojApplication.java
@@ -1,11 +1,13 @@
 package com.qmq.springbootpoj;
 
+import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @MapperScan("com.qmq.springbootpoj.mapper")
+@EnableApolloConfig
 public class SpringBootPojApplication {
 
     public static void main(String[] args) {
